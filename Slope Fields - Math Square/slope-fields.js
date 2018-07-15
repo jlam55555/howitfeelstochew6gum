74576f1819a2fc10ee666c876ab25e64d7e433ef
Poobaloofa <julian.ivaldi@gmail.com>
2018-07-14 21:20:56 -0400

    Add files via upload

--- Slope Fields - Math Square/slope-fields.js
+++ Slope Fields - Math Square/slope-fields.js
@@ -13,6 +13,8 @@ import P5Behavior from 'p5beh';
 
 const pb = new P5Behavior();
 
+var bounds = 6;
+
 // for WEBGL: pb.renderer = 'webgl';
 
 function dydx(x,y){ //comment out any functions not being used
@@ -35,19 +37,19 @@ pb.draw = function (floor, p) {
   /* this == pb.p5 == p */
   // draw here...
 
-  var ugX = this.width/12;
-  var ugY = this.height/12;
+  var ugX = this.width/(2*bounds);
+  var ugY = this.height/(2*bounds);
 
   this.clear();
   this.fill(255,255,255);
-  for(var x = -6; x<=6; x++){
-    for(var y = -6; y<=6; y++){
+  for(var x = -bounds; x<=bounds; x++){
+    for(var y = -bounds; y<=bounds; y++){
       var tempAng = this.atan2(dydx(x,y),1);
       this.stroke(127,127,127);
       this.strokeWeight(2);
-      this.line(ugX*(6+x)-10*Math.cos(tempAng),ugY*(6+y)-10*Math.sin(tempAng),ugX*(6+x)+10*Math.cos(tempAng),ugY*(6+y)+10*Math.sin(tempAng));
+      this.line(ugX*(bounds+x)-10*Math.cos(tempAng),ugY*(bounds+y)-10*Math.sin(tempAng),ugX*(bounds+x)+10*Math.cos(tempAng),ugY*(bounds+y)+10*Math.sin(tempAng));
       this.noStroke();
-      this.ellipse(ugX*(6+x),ugY*(6+y),5,5);
+      this.ellipse(ugX*(bounds+x),ugY*(bounds+y),5,5);
     }
   }
   function euler(x,y,step){
@@ -55,16 +57,16 @@ pb.draw = function (floor, p) {
     var euly = y;
     p.stroke(255,0,0);
     p.strokeWeight(2);
-    while(eulx>=-6){
-      p.line(ugX*(6+eulx),ugY*(6+euly),ugX*(6+eulx-step),ugY*(6+euly-dydx(eulx,euly)*step));
+    while(eulx>=-bounds){
+      p.line(ugX*(bounds+eulx),ugY*(bounds+euly),ugX*(bounds+eulx-step),ugY*(bounds+euly-dydx(eulx,euly)*step));
       euly = euly - dydx(eulx,euly)*step;
       eulx = eulx-step
     }
     eulx = x;
     euly = y;
 
-    while(eulx<=6){
-      p.line(ugX*(6+eulx),ugY*(6+euly),ugX*(6+eulx+step),ugY*(6+euly+dydx(eulx,euly)*step));
+    while(eulx<=bounds){
+      p.line(ugX*(bounds+eulx),ugY*(bounds+euly),ugX*(bounds+eulx+step),ugY*(bounds+euly+dydx(eulx,euly)*step));
       euly = euly+dydx(eulx,euly)*step;
       eulx = eulx+step
     }
@@ -74,7 +76,7 @@ pb.draw = function (floor, p) {
 
   for (let u of floor.users) {
     pb.drawUser(u);
-    euler(u.x*12/p.width-6,u.y*12/p.height-6,0.01);
+    euler(u.x*(2*bounds)/p.width-bounds,u.y*(2*bounds)/p.height-bounds,0.01);
   }
   this.fill(128, 128, 128, 128);
   this.noStroke();
